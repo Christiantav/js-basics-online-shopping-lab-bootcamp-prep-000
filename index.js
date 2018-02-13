ca4a9bc0bb1a1100cb3bb510b85d6bdf76958981
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-13 07:02:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ else if(cart.length >= 3) {
   console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])},
   ${Object.keys(cart[1])} at $${Object.values(cart[1])}, and ${Object.keys(cart[2])} at $${Object.values([cart2])}.`)
 }
+}
 
 
 
