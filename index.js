f56a736c304110b29b21b6957f7bf5befbe82788
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-13 06:47:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ else if(cart.length >= 3) {
   var bigCart = []
   for (var i = 0; i < cart.length; i++) {
     for (var item in cart[i]) {
-      bigCart.push(`${item} at $${Object.values(cart[i])}`)    
+      bigCart.push(`${item} at $${Object.values(cart[i])}`)
 }
 }
 console.log(`In your cart, you have ${items.join(", ")}.`
