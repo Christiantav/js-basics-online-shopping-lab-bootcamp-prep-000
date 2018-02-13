93ee7d4c00d9a926c8ab6cba69d59a09ac71bf07
Christian Tavares <chrisadrianotav@gmail.com>
2018-02-13 07:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ else if(cart.length === 2){
   console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)
 }
 else {
-  console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])},
-  ${Object.keys(cart[1])} at $${Object.values(cart[1])}, and ${Object.keys(cart[2])} at $${Object.values(cart[2])}.`)
+  console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}, ${Object.keys(cart[1])} at $${Object.values(cart[1])}, and ${Object.keys(cart[2])} at $${Object.values(cart[2])}.`)
 }
 }
 
